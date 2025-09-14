266d186508eaec9874099755b138d2168a170ebd
Vaibhav Upadhyay <vaibhavxlr@gmail.com>
2025-09-14 17:37:49 +0530

    fixes

--- ssg/model.go
+++ ssg/model.go
@@ -3,24 +3,28 @@ package ssg
 import "html/template"
 
 type AboutPage struct {
-	SiteTitle string
-	Author    string
-	Year      int
-	Content   template.HTML
+	SiteTitle     string
+	Author        string
+	CopyrightYear int
+	Content       template.HTML
 }
 
 type Post struct {
-	Title   string
-	Date    string
-	Slug    string
-	Year    int
-	Content template.HTML
+	SiteTitle     string
+	Title         string
+	Date          string
+	Slug          string
+	Year          int
+	Content       template.HTML
+	Author        string
+	CopyrightYear int
 }
 
 type BlogPage struct {
-	SiteTitle   string
-	BlogIntro   string
-	Author      string
-	Year        int
-	PostsByYear map[int][]Post
+	SiteTitle     string
+	BlogIntro     string
+	Author        string
+	CopyrightYear int
+	Years         []int
+	PostsByYear   map[int][]Post
 }
--- ssg/ssg.go
+++ ssg/ssg.go
@@ -1,13 +1,13 @@
 package ssg
 
 import (
-	"fmt"
 	"html/template"
 	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -41,8 +41,8 @@ func deserializeContent(front, body, contType string) any {
 		pageVals.Content = template.HTML(htmlBody)
 		pageVals.Author = "Vaibhav Upadhyay"
 		pageVals.SiteTitle = "vaibhavxlr's blog"
-		pageVals.Year = time.Now().Year()
-		fmt.Printf("struct - %v", pageVals)
+		pageVals.CopyrightYear = time.Now().Year()
+		log.Printf("[INFO] about struct - %v", pageVals)
 		return pageVals
 
 	case "blog":
@@ -50,9 +50,9 @@ func deserializeContent(front, body, contType string) any {
 		var pageVals BlogPage
 		pageVals.Author = "Vaibhav Upadhyay"
 		pageVals.SiteTitle = "vaibhavxlr's blog"
-		pageVals.Year = time.Now().Year()
-		pageVals.PostsByYear = nil
-		fmt.Printf("struct - %v", pageVals)
+		pageVals.CopyrightYear = time.Now().Year()
+		pageVals.BlogIntro = "I write about things which I find interesting"
+		log.Printf("[INFO] blog struct - %v", pageVals)
 		return pageVals
 
 	case "post":
@@ -63,9 +63,12 @@ func deserializeContent(front, body, contType string) any {
 			log.Fatalf("Failed to parse front matter - 'post' level: %v", err)
 		}
 		htmlBody := markdown.ToHTML([]byte(body), nil, nil)
+		pageVals.Author = "Vaibhav Upadhyay"
+		pageVals.SiteTitle = "vaibhavxlr's blog"
+		pageVals.CopyrightYear = time.Now().Year()
 		pageVals.Year, _ = strconv.Atoi(strings.Split(pageVals.Date, "-")[0])
 		pageVals.Content = template.HTML(htmlBody)
-		fmt.Printf("struct - %v", pageVals)
+		log.Printf("[INFO] post struct - %v", pageVals)
 		return pageVals
 
 	}
@@ -105,7 +108,10 @@ func Generate(content, htmlTemplatesPath, output string) {
 				log.Fatalf("[ERROR] unable to create 'post' dir: %v", err)
 			}
 			outputFile, _ := os.Create(filepath.Join(dir, "index.html"))
-			tmpl.ExecuteTemplate(outputFile, "post", pageCont)
+			err = tmpl.ExecuteTemplate(outputFile, "post", pageCont)
+			if err != nil {
+				log.Fatalf("[ERROR] failed to execute 'post' template: %v", err)
+			}
 
 			postbyYear[pageCont.Year] = append(postbyYear[pageCont.Year], pageCont)
 			srcDir := filepath.Dir(path)
@@ -128,14 +134,21 @@ func Generate(content, htmlTemplatesPath, output string) {
 		log.Fatalf("[ERROR] unable to create 'blog' dir: %v", err)
 	}
 	outputFile, _ := os.Create(filepath.Join(output, "blog", "index.html"))
-
 	pageCont := deserializeContent("", "", "blog").(BlogPage)
-
-	fmt.Printf("\npageCont: %v", pageCont)
+	var years []int
+	for year := range postbyYear {
+		years = append(years, year)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(years)))
+	for _, year := range years {
+		posts := postbyYear[year]
+		sort.Slice(posts, func(i, j int) bool {
+			return posts[i].Date > posts[j].Date
+		})
+		postbyYear[year] = posts
+	}
+	pageCont.Years = years
 	pageCont.PostsByYear = postbyYear
-
-	fmt.Printf("\npostbytyear2: %v", postbyYear)
-	fmt.Printf("\npageCont2: %v", pageCont)
 	err = tmpl.ExecuteTemplate(outputFile, "blog", pageCont)
 	if err != nil {
 		log.Fatalf("[ERROR] error executing 'blog' template %v", err)
